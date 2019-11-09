bf2879a4a6f26382405d11d0043bb484ec17b8ad
Henry Phan <h.phan06@gmail.com>
2019-11-09 04:04:38 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+require 'pry'
+
+def my_collect(array)
+  counter = 0 
+  empty_array = []
+  while counter < array.length do 
+    empty_array << yield(array[counter])
+    counter += 1
+  end
+  empty_array
+end
 
 
