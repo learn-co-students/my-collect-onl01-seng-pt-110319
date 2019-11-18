7e94ec3e9b21d0d0137fa9ec33d32d2ccec29323
Jordan Hayes <nosafoundation@gmail.com>
2019-11-18 04:30:01 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,9 @@
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-
-my_collect(array) do |name|
-  name.split(" ").first
-end
-
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
-  lang.upcase
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+  end
+  collect
 end
