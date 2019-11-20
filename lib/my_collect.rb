f4c91a78118f0642047e97481b0fa01146936d87
Henry Phan <h.phan06@gmail.com>
2019-11-20 01:08:45 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,19 @@
 require 'pry'
 
+# def my_collect(array)
+#   counter = 0 
+#   empty_array = []
+#   while counter < array.length do 
+#     empty_array << yield(array[counter])
+#     counter += 1
+#   end
+#   empty_array
+# end
+
 def my_collect(array)
-  counter = 0 
-  empty_array = []
-  while counter < array.length do 
-    empty_array << yield(array[counter])
-    counter += 1
+  new_array = []
+  array.map do |language|
+    new_array << yield(language)
   end
-  empty_array
+  new_array
 end
-
-
