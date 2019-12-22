cf12d714c97de249a1ceb0d6b9e5013b82ce7bbf
Hussain Ahmed <haz6798@icloud.com>
2019-12-22 04:23:48 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def hello(array)
+  i = 0	  i = 0
+  collection = []
+  while i < array.length	  while i < array.length
+    yield(array[i])	    collection << yield(array[i])
+    i += 1	    i += 1
+  end	  end
+  collection
+end	end
 
 
+
+
+hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }	hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
